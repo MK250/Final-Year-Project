8247e435580cd3489c6fe28ef401489313969047
MK250 <77729467+MK250@users.noreply.github.com>
2024-02-29 15:54:19 +0000

    Add files via upload

--- sugarsync/BolusAdvisorActivity.java
+++ sugarsync/BolusAdvisorActivity.java
@@ -4,15 +4,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class BolusAdvisorActivity extends AppCompatActivity {
+
+    private LinearLayout youtubePlayerContainer;
 
     private EditText editTextGlucose, editTextCarbs, editTextISF;
     private Button buttonCalculate;
     private TextView textViewResult;
+
+    private List<String> playlist = Arrays.asList(
+            "povArl9xZSg");
+
+    private int currentVideoIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,9 @@ public class BolusAdvisorActivity extends AppCompatActivity {
         editTextISF = findViewById(R.id.editTextISF);
         buttonCalculate = findViewById(R.id.buttonCalculate);
         textViewResult = findViewById(R.id.textViewResult);
+        youtubePlayerContainer = findViewById(R.id.youtube_player_view);
+
+        refreshYouTubeVideo();
 
         // Set onClickListener for the calculate button
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +52,21 @@ public class BolusAdvisorActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void refreshYouTubeVideo() {
+        YouTubePlayerView youTubePlayerView = new YouTubePlayerView(this); // or use getContext() instead of this
+        youtubePlayerContainer.removeAllViews();
+        youtubePlayerContainer.addView(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer) {
+                youTubePlayer.loadVideo(playlist.get(currentVideoIndex), 0);
+                currentVideoIndex = (currentVideoIndex + 1) % playlist.size();
+            }
+        });
+    }
+
 
     private void calculateBolus() {
         // Get inputs
